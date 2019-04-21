07c5a046d52f6ff34f80b9ad99a328c0b464e369
Michael Hu <pxmyhdev@gmail.com>
2019-04-21 12:09:42 -0400

    add heroku deployment

--- app/echo.py
+++ app/echo.py
@@ -17,4 +17,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    print('Starting echo ...')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
